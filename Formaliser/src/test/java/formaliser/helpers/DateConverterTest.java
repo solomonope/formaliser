e23f174f14d708c4060a44e8db6ad2080d4a63b1
mwanji <mwanji@ea41db64-bdb9-11de-a0e9-83d03c287164>
2009-11-11 23:45:37 +0000

    Tests migrated from Meow

--- Formaliser/src/test/java/formaliser/helpers/DateConverterTest.java
+++ Formaliser/src/test/java/formaliser/helpers/DateConverterTest.java
@@ -5,7 +5,6 @@ import static org.fest.assertions.Assertions.assertThat;
 import java.util.Calendar;
 import java.util.Date;
 
-import org.apache.commons.lang.time.DateUtils;
 import org.junit.Test;
 
 public class DateConverterTest {
@@ -13,25 +12,53 @@ public class DateConverterTest {
     private DateConverter converter = new DateConverter();
     
     @Test
-    public void converts_date_and_time() {
-        
-        Date date = converter.convert("19/03/2008 15:32");
-        
-        Calendar expectedCal = Calendar.getInstance();
-        expectedCal.set(2008, Calendar.MARCH, 19, 15, 32);
-        expectedCal = DateUtils.truncate(expectedCal, Calendar.MINUTE);
-        
-        assertThat(date).isEqualTo(expectedCal.getTime());
+    public void can_convert_java_util_Date() throws Exception {
+        assertThat(converter.canConvert(Date.class)).isTrue();
     }
     
     @Test
-    public void converts_sql_date() {
-        long now = System.currentTimeMillis();
-        
+    public void converts_date_and_time() {
+        assertThat(converter.convert("19/03/2008 15:32")).isEqualTo(date(2008, Calendar.MARCH, 19, 15, 32));
+    }
+    
+    @Test
+    public void converts_java_sql_date() {
         assertThat(converter.canConvert(java.sql.Date.class)).isTrue();
         
-        String expected = converter.convert(new java.sql.Date(now));
+        long now = System.currentTimeMillis();
         
-        assertThat(expected).isEqualTo(converter.convert(new Date(now)));
+        assertThat(converter.convert(new java.sql.Date(now))).isEqualTo(converter.convert(new Date(now)));
+    }
+    
+    @Test
+    public void converts_dd_mm_yyyy_date_format() throws Exception {
+        assertThat(converter.convert("01/02/2009")).isEqualTo(date(2009, Calendar.FEBRUARY, 1));
+    }
+
+    @Test
+    public void converts_dd_mm_yyyy_hh_mm_date_format() throws Exception {
+        assertThat(converter.convert("01/02/2009 12:31")).isEqualTo(date(2009, Calendar.FEBRUARY, 1, 12, 31));
+    }
+    
+    @Test
+    public void converts_invalid_date_to_null() throws Exception {
+        assertThat(converter.convert("01/13/2009")).isNull();
+
+        assertThat(converter.convert("03/2009")).isNull();
+
+        assertThat(converter.convert("03/2010 15:31")).isNull();
+    }
+    
+    private Date date(int year, int month, int day) {
+        return date(year, month, day, 0, 0);
+    }
+    
+    private Date date(int year, int month, int day, int hour, int minute) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setLenient(false);
+        calendar.set(year, month, day, hour, minute, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        
+        return calendar.getTime();
     }
 }
